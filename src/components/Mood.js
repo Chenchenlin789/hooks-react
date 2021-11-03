1443ab682066ca1a2499b857c43298d4e3ea36e0
Chenchenlin789 <chenchenlihn@gmail.com>
2021-11-03 18:36:20 +0100

    hook1

--- src/components/Mood.js
+++ src/components/Mood.js
@@ -8,7 +8,7 @@ const Mood = () => {
     const handleMood = () => {
         setMood(!true)
     }
-    const catface = () => <Cat size={200} mood={mood ? "blissful" : "sad"} color="#FDA7DC" />;
+    const catface = () => <Ghost size={200} mood={mood ? "blissful" : "sad"} color="#FDA7DC" />;
     return (
         <div>
             <div>
@@ -17,5 +17,43 @@ const Mood = () => {
         </div >
     );
 }
+
+
+// import { Cat } from 'react-kawaii';
+// import { Ghost } from 'react-kawaii'
+// import {usestate} from 'react'
+
+// const Mood = () => {
+//     const[isLucky,setMood]=usestate(true);
+
+//     const toggleMood =()=>{
+//         setMood(!isLucky)
+//     }
+//     const[isMood,randomMood]=usestate(["sad", "shocked", "happy", "blissful", "lovestruck", "excited", "ko"]);
+
+//     const getRandomMood=()=>{
+//         let random =Math.floor(Math.random() * 8);
+//         randomMood(isMood[random])
+
+//     }
+//     return ( 
+//     <div>
+//         <div>
+//             <Cat size={200} mood={isLucky?"blissful":"sad"} color="#FDA7DC" />;
+//             <button onClick={toggleMood}>
+//                 Change Mood
+//             </button>
+//         </div>
+//         <div>
+//         <Ghost size={200} mood={isMood} color="#FDA7DC" />;
+//         <button onClick={getRandomMood}>
+//             Random Mood
+//         </button>
+//         </div>
+//     </div>
+//      );
+// }
+
+// export default Mood;
 
 export default Mood;
